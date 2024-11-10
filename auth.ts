1c976d7c8595815ad4e78bc706baded8767786d3
Shazid <shazid.dev@gmail.com>
2024-11-10 18:26:02 +0600

    prisma with mongodb setup

--- auth.ts
+++ auth.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GitHub from "next-auth/providers/github";
 
+import { db } from "@/lib/db";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt", // Use JWT for stateless sessions in MongoDB
+  },
   providers: [GitHub],
 });
